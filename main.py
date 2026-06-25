659ebd984b7aef8465e103efcda7933ee9819215
MIKAIL7787 <tort101to@gmail.com>
2026-06-26 00:33:33 +0300

    main.py1

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print("Проект для тренировки веток Git")
 from profile import print_profile
 print("Student Branching App")
 print_profile()
+from profile import print_profile
+from subjects import print_subjects
+print("Student Branching App")
+print_profile()
+print_subjects()
